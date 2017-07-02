f0925554d6516cca9e2643367441e86f79af35cd
Rodel Ruiz <rodelruiz@Rodels-MacBook-Pro.local>
2017-07-02 22:27:43 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5957a64f87e942db553ad26a')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5956174eb5bcf704713f788b')
+    }, {
+        $set: {
+            name: 'Rodel'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+
+
+
+    // db.close();
+
+});
